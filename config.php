d9c6e172933203428a30b014121415151dd11677
Yiming Zong <yzong@cmu.edu>
2015-04-12 14:17:33 -0400

    Initial commit for cmupostly.

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+# PUT YOUR SITE'S ADDRESS HERE
+$home = "http://www.contrib.andrew.cmu.edu/~yzong/cmupostly/";
+
+$MY_HOST = "contrib-postgres.club.cc.cmu.edu";
+$MY_DB_PORT = 5432;
+
+# FILL IN THE INFO FOR YOUR DATABASE BELOW
+$MY_DB = "contrib_yzong"; # DATABASE NAME
+$DB_USER = "yzong"; # DATABASE USERNAME
+$DB_PW = "yzong@andrew"; # DATABASE PASSWORD
+
+
+
+
+# Don't need to change
+
+
+$salt = "15-501";
+
+?>
+
